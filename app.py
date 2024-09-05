b4e88b3ba1a8b140730ddb19915caccfe286ffeb
AnnyC28 <160602515+AnnyC28@users.noreply.github.com>
2024-09-05 10:36:09 -0300

    aula 05-09

---
+++ app.py
@@ -0,0 +1,9 @@
+print('Aurora confecções\n')
+
+print('1. Cadastro de confecções de vestidos de noiva')
+print('2. Listar horários de produção de vestidos agendados')
+print('3. Ativar cadastro de cliente com vestidos em produção')
+print('4. Sair da aplicação')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print('Você escolheu a opção: ' , opcao_escolhida)
