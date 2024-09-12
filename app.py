d61e2d17d0547cd0dbc3808152b6338c4ba0ddbd
AnnyC28 <160602515+AnnyC28@users.noreply.github.com>
2024-09-12 10:34:53 -0300

    site_Python

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-print('Aurora confecções\n')
+import os
 
-print('1. Cadastro de confecções de vestidos de noiva')
-print('2. Listar horários de produção de vestidos agendados')
-print('3. Ativar cadastro de cliente com vestidos em produção')
-print('4. Sair da aplicação')
+def mostra_titulo():
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção: ' , opcao_escolhida)
+    print('Aurora confecções\n')
+
+def mostra_escolhas():
+    print('1. Cadastro de confecções de vestidos de noiva')
+    print('2. Listar horários de produção de vestidos agendados')
+    print('3. Ativar cadastro de cliente com vestidos em produção')
+    print('4. Sair da aplicação')
+
+def escolhe_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print('Você escolheu a opção: ' , opcao_escolhida)
+
+    def finalizar_programa():
+        os.system('cls')
+        print('Finalizando programa')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar vestidos para confecção')
+    elif opcao_escolhida == 2:
+        print('Listar horários e consultas de orçamento agendados')
+    elif opcao_escolhida == 3:
+        print('Ativar/Desativar cadastro de clientes')
+    else:
+        finalizar_programa()
+
+def main():
+    mostra_titulo()
+    mostra_escolhas()
+    escolhe_opcao()
+
+if __name__ == '__main__':
+    main()
